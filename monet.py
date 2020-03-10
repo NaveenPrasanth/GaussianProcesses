c1283f3c47a24e9584c2a9196d01f7bd67beea06
Suriyadeepan Ramamoorthy <suriyadeepan.r@gmail.com>
2020-03-10 19:31:59 +0530

    adds helpers for pretty plot

---
+++ monet.py
@@ -0,0 +1,14 @@
+cerulean = '#00BEFF'
+oldgold = '#D4CA3A'
+hotpink = '#FF6DAE'
+aquablue = '#67E1B5'
+mauve = '#EBACFA'
+seapink = '#F1988E'
+fern = '#5DB15A'
+burntsienna = '#E28544'
+tradewind = '#52B8AA'
+silverchalice = '#9E9E9E'
+black = '#1B2631'  # not gadfly
+
+colors = [ cerulean, oldgold, hotpink, aquablue, mauve, silverchalice, seapink, fern, burntsienna, tradewind ]
+
